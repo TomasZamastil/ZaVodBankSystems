b842f144a8671f2a29bed4e8cdc6e6bdcaae54b0
TomasZamastil <thomas.zamastil@gmail.com>
2026-04-03 13:32:25 +0200

    App starter
    starting the development

---
+++ src/main/java/com/example/zavodbanksystems/ZaVodBankSystemsApplication.java
@@ -0,0 +1,13 @@
+package com.example.zavodbanksystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaVodBankSystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaVodBankSystemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zavodbanksystems/ZaVodBankSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zavodbanksystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaVodBankSystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
